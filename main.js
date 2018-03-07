f3cc559e2716eefb96155154de150f90ff7806c2
CasterQi <zh740193602@163.com>
2018-03-07 19:30:22 +0800

    homewoek1

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ http.createServer(function(req,res){
     res.write('ERROR 404:NOT FOUND')
     res.end('interesting')
 }).listen(3000)
+function asyncFun(args,callback){
+    var result = args + 'aaaa';
+    callback(result);
+}
+
+asyncFun("interesting",function(result){
+    console.log(result);
+})
+
+var fs = require('fs')
+var path = require('path')
+var extName = '.js'
+fs.readdir('/vue-devtools-master',function(err,files){
+    if (err) {
+        return console.error(err);
+    }
+    files.forEach( function (file){
+        if(path.extname(file)==extName){
+            console.log( file );
+
+        }
+        
+    });
+})
